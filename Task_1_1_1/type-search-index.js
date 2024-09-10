20cf7eecb95f2cbba8eb5d2a09309d180dc8aa5e
VisTheB <VisTheB@users.noreply.github.com>
2024-09-10 17:53:21 +0000

    Deploying to gh-pages from @ VisTheB/OOP@198b7a24bd7e63ac408539e06011af0866699586 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.basargina","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.basargina","l":"HeapSort"}]
