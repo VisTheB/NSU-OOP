f8326157da97010bab9f10b878ba25a97e91c293
VisTheB <VisTheB@users.noreply.github.com>
2024-09-29 15:44:13 +0000

    Deploying to gh-pages from @ VisTheB/OOP@91f1d394e85163bdd8755afbe260c639b1132a13 🚀

---
+++ Task_1_1_3/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.basargina"},{"l":"ru.nsu.basargina.actions"},{"l":"ru.nsu.basargina.data"}];updateSearchResults();
