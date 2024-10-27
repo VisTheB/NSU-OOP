89a6607635b7ae9b61e26a3dc34c6119c4cc8013
VisTheB <VisTheB@users.noreply.github.com>
2024-10-27 14:50:55 +0000

    Deploying to gh-pages from @ VisTheB/OOP@d1a694d2b78bebffacaae5fbfc91c7b86f7a3c4f 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.basargina","l":"Entry"},{"p":"ru.nsu.basargina","l":"HashTable"}];updateSearchResults();
